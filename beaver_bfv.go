ad6f0e64954e612846052ba9d46a56eb4777a05d
Moodoo245 <justinas.sukaitis@epfl.ch>
2020-03-21 21:41:14 +0100

    moved some comments around to understand better

--- beaver_bfv.go
+++ beaver_bfv.go
@@ -7,44 +7,6 @@ import (
 	"github.com/ldsec/lattigo/ring"
 )
 
-//each Party i does
-/*
-	a_i <- Z^n _t 		sampling of a uniform vector of integers mod p of n elements
-	b_i <- Z^n _t
-	c_i = a_i x b_i (component wise)
-	encode a_i to ring R = Z_Q[X]/X^N + 1
-	encode b_i to ring R
-	sk_i <- xi_(1/3)    sampling of elem of R with ternary coeff [-1 0 1] with distrib [(2/6), 1/3, 2/6]
-
-	di = Enc_sk_i (a_i) in R^2
-	foreach other party j
-		send di to j
-
-
-*/
-//each Party i does
-/*   foreach other party j do
-receive dj from j
-r_ij <- Z^n _t
-c_i = c_i - r_ij
-encode r_ ij = to ring R
-(e^0_ij, e^ij) <- xi _err in R^2
-d_ij = Add(Mul(d_j, bi), r_ij) + (e^o _ij, e^ _ij)
-send d_ij to Pj
-
-*/
-//each Party i does
-/*
-	c' = (0,0) in R^2
-	foreach other party j
-		d _ji receive from j
-		c' = add(c', d_ij)
-
-	c' = Dec_sk(c')
-	decode from R c'
-	c_i = c_i + c'
-*/
-
 //BeaverProtocol stores all data that is reused between 2 runs
 type BeaverProtocol struct {
 	*LocalParty
@@ -59,7 +21,7 @@ type BeaverProtocol struct {
 	sk     uint64
 }
 
-//same as remoteParty
+//!!!same as remoteParty!!!
 /*
 type BeaverRemoteParty struct {
 	*RemoteParty
@@ -111,15 +73,44 @@ func (lp *LocalParty) New(n uint64, T uint64) *BeaverProtocol {
 	bep.b = ring.NewPoly(uint64(math.Pow(2, 13)), bep.params.Qi[0])
 	bep.b.SetCoefficients([][]uint64{b})
 
+	//sk <- xi_(1/3)
 	return bep
 }
 
 //BeaverRun runs beaver prot
 func BeaverRun() {
+
+	/*
+		di = Enc_sk_i (a_i) in R^2
+		foreach other party j
+		send di to j
+	*/
+
+	/*   foreach other party j do
+	receive dj from j
+	r_ij <- Z^n _t
+	c_i = c_i - r_ij
+	encode r_ ij = to ring R
+	(e^0_ij, e^ij) <- xi _err in R^2
+	d_ij = Add(Mul(d_j, bi), r_ij) + (e^o _ij, e^ _ij)
+	send d_ij to Pj
+
+	*/
+	//each Party i does
+	/*
+		c' = (0,0) in R^2
+		foreach other party j
+			d _ji receive from j
+			c' = add(c', d_ij)
+
+		c' = Dec_sk(c')
+		decode from R c'
+		c_i = c_i + c'
+	*/
 	return
 }
 
-//same as given
+//!!!same as given!!!
 /*
 func BindNetwork() {
 	return
