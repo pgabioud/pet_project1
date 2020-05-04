5aa5e1b9b150f03fdf80372c7b7d1122ba74b9fb
pgabioud <p.gabioud@epfl.ch>
2020-05-04 18:06:44 +0200

    registration complete + sanitizing/prettyprint of the code

--- pet_project2/skeleton/credential.py
+++ pet_project2/skeleton/credential.py
@@ -10,10 +10,9 @@
 from serialization import jsonpickle
 import random as rd
 import petrelic
-from petrelic.multiplicative.pairing import G1, G2
+import hashlib
+from petrelic.multiplicative.pairing import G1, G2, GT
 class PSSignature(object):
-
-    L = 2
     """PS's Multi-message signature from section 4.2
     
     **Important** This class has no direct use in the project.
@@ -23,11 +22,14 @@ class PSSignature(object):
     misunderstandings/problems early on.
     """
 
+    #Assume:
+    L = 2
+
     def generate_key(self):
         g = G2.generator()
         sk = []
         pk = []
-        for i in range(self.L + 1):
+        for _ in range(self.L + 1):
             y = petrelic.bn.Bn.from_num(rd.randint(1, G2.order()))
             sk.append(y)
             pk.append(g**y)
@@ -69,21 +71,23 @@ class Issuer(object):
             valid_attributes (string): all valid attributes. The issuer
             will never be called with a value outside this list
         """
-        # (p, g, G1, G2, GT, our u)
+        # (p, g, g tilde, G1, G2, GT, secret key, public key tilde, public key)
         self.g = G1.generator()
         self.gt = G2.generator()
         self.G1 = G1
         self.G2 = G2
-        self.GT = petrelic.multiplicative.pairing.GT
+        self.GT = GT
         self.valid_attributes = valid_attributes
         self.sk = []
         self.pkt = []
         self.pk = []
-        for i in range(len(valid_attributes) + 1):
+        for _ in range(len(valid_attributes) + 1):
             y = petrelic.bn.Bn.from_num(rd.randint(1, self.G2.order()))
             self.sk.append(y)
             self.pk.append(self.g**y)
             self.pkt.append(self.gt**y)
+
+        print("Setup done")
 
 
     def get_serialized_public_key(self):
@@ -122,19 +126,32 @@ class Issuer(object):
         (AnonCredential.create_issue_request), and a list of known attributes of the
         user (e.g. the server received bank notes for subscriptions x, y, and z).
 
-        You should design the issue_request as you see fit.
-        """
-        rd.seed(user)
-        revealed_attr = revealed_attr.split(",")
-        u = petrelic.bn.Bn.from_num(rd.randint(1, server_sk.get("public_params").get("G1").order()))
-        X = server_sk.get("X")
-        sigma2 = X * C
-        for i in range(len(server_sk.get("public_params").get("valid_attr"))):
-            if revealed_attr[i] != 'X':
-                Y = server_sk.get("public_params").get("pk")[i]
-                sigma2 *= Y ** petrelic.bn.Bn.from_num(int(revealed_attr[i]))
+        Args:
+            C (G1Element): commit from user
+            user (string): username of the user
+            revealed_attr (list): list of revealed attributes of the user
+            server_sk (dict): the server's secret key and other parameters
 
-        sigma = [server_sk.get("public_params").get("g") ** u, sigma2 ** u]
+        Returns:
+            sigma (tuple(G1Element, G1Element)): credentials of the issuer
+        """
+
+        rd.seed(user)
+        server_pb_params = server_sk.get("public_params")
+
+        # (u, X)
+        u = petrelic.bn.Bn.from_num(rd.randint(1, server_pb_params.get("G1").order()))
+        X = server_sk.get("X")
+
+        # create credentials sigma'
+        sigma2 = X * C
+        for i in range(len(revealed_attr)):
+            Y = server_pb_params.get("pk")[i]
+            sigma2 *= Y ** petrelic.bn.Bn.from_num(int(revealed_attr[i]))
+
+        sigma = [server_pb_params.get("g") ** u, sigma2 ** u]
+
+        print("Credentials created")
         return sigma
 
 
@@ -145,11 +162,34 @@ class AnonCredential(object):
         """Gets all known attributes (subscription) of a user and creates an issuance request.
         You are allowed to add extra attributes to the issuance.
 
-        You should design the issue_request as you see fit.
+        Args:
+            private_attributes (list): list of private attributes of the client
+            G1 (petrelic group): group of the issueer private key
+            Y (G1Element[]): public key of the issuer
+            g (G1Element): generator of G1
+            t (Bn): random big number between 1 and order of G1
+            pub_attr_len (int): number of public attributes of the client
+
+        Returns:
+            C (G1Element): commit of the client
+            gamma (G1Element[]): list of gamma used for zero knowledge proof of commit
+            r (Bn): list of exponent for zero knowledge proof of commit
         """
+
+        # Commit is C = g^t * Y1^a1 * Y2*a2 * ...
+
+        # Zero Knowledge Proof:
+        # - pick v0 on G1, compute gamma0 = g^v0
+        # - pick vi on G1, compute gammai = Yi^vi for i from 1 to nb of private_attr
+        # - compute c = Hash(C, g, Y1, Y2, ..., gamma0, gamma1, gamma2)
+        # - compute r0 = v0 - c*t
+        # - compute ri = vi - c*ai for i from 1 to nb of private_attr
+        # => check: gamma0 * gamma1 * gamma2 * ... = C^c * g^r0 * Y1^r1 * Y2^r2 * ...
+
         C = g**t
         v = []
         gamma = []
+        
         v0 = petrelic.bn.Bn.from_num(rd.randint(1, G1.order()))
         v.append(v0)
         gamma.append(g**v0)
@@ -159,26 +199,25 @@ class AnonCredential(object):
             v.append(vi)
             gamma.append(Y[i+pub_attr_len]**vi)
         
-        to_hash = [C, g]
+        to_hash = C.to_binary() + g.to_binary()
         for i in range(len(private_attributes)):
-            to_hash.append(Y[i+pub_attr_len])
-
+            to_hash += Y[i+pub_attr_len].to_binary()
         for gammai in gamma:
-            to_hash.append(gammai)
+            to_hash += gammai.to_binary()
 
-        # c = H(C, g, Y1, Y2, ..., gamma0, gamma1, gamma2)
-        c_hash = hash(tuple(to_hash))
+        c_hash = int(hashlib.sha512(to_hash).hexdigest(), 16)
         
-        
-        
-        r = []
-        for vi, a in zip(v, private_attributes):
-            res = (vi - c_hash*int(a))
-            
-            if  res < 0:
-                res = res % G1.order()
-                
-            r.append(res)
+        r0 = v0 - c_hash*t
+        if r0 < 0:
+            r0 = r0 % G1.order()
+        r = [r0]
+        for vi, a in zip(v[1:], private_attributes):
+            ri = (vi - c_hash*int(a))
+            if  ri < 0:
+                ri = ri % G1.order()
+            r.append(ri)
+
+        print("Issue request created")
         return C, gamma, r
 
         
--- pet_project2/skeleton/your_code.py
+++ pet_project2/skeleton/your_code.py
@@ -7,6 +7,7 @@ from serialization import jsonpickle
 import credential
 import petrelic
 import random as rd
+import hashlib
 class Server:
     """Server"""
     
@@ -30,11 +31,13 @@ class Server:
             You are free to design this as you see fit, but all commuincations
             needs to be encoded as byte arrays.
         """
+
         Issuer = credential.Issuer()
         Issuer.setup(valid_attributes.split(","))
         pub = Issuer.get_serialized_public_key()
         sec = Issuer.get_serialized_secret_key()
 
+        print("CA generated")
         return pub, sec
 
     @staticmethod
@@ -42,7 +45,7 @@ class Server:
         """ Registers a new account on the server.
 
         Args:
-            server_sk (byte []): the server's secret key (serialized)
+            server_sk (byte []): the server's secret key (serialized) and other parameters
             issuance_request (bytes[]): The issuance request (serialized)
             username (string): username
             attributes (string): attributes
@@ -53,21 +56,53 @@ class Server:
             response (bytes[]): the client should be able to build a credential
             with this response.
         """
-        print(attributes)
+
+        attributes_list = attributes.split(',')
         server_sk = jsonpickle.decode(server_sk.decode("utf-8"))
+        server_pb_params = server_sk.get("public_params")
         issuance_request = jsonpickle.decode(issuance_request)
 
+        # Zero Knowledge Proof verification on client commit
+        C = issuance_request.get("C")
+        r = issuance_request.get("r")
+        gamma = issuance_request.get("gamma")
+        g = server_pb_params.get("g")
+        Y = server_pb_params.get("pk")
 
-        sigma = credential.Issuer.issue(issuance_request.get("C"), username, attributes + ",X", server_sk)
+        to_hash = C.to_binary() + g.to_binary()
+        for i in range(len(gamma)-1):
+            to_hash += Y[i+len(attributes_list)].to_binary()
+        for gammai in gamma:
+            to_hash += gammai.to_binary()
+
+        c_hash = int(hashlib.sha512(to_hash).hexdigest(), 16)
+
+        gamma_prod = server_pb_params.get("G1").neutral_element()
+        for gammai in gamma:
+            gamma_prod *= gammai
+
+        check_prod = (C**c_hash)*(g**r[0])
+        for i in range(len(r)-1):
+            check_prod *= Y[i+len(attributes_list)]**r[i+1]
         
-        return bytearray(jsonpickle.encode(sigma), 'utf-8')
-    def check_request_signature(
-        self, server_pk, message, revealed_attributes, signature
-    ):
+        proof_correct = (gamma_prod == check_prod)
+
+        # if proof incorrect, return empty byte array
+        if not proof_correct:
+            print("ZKP incorrect: cancel registration")
+            return bytearray()
+        # if proof correct, return issuer credentials
+        else:
+            print("ZKP correct: proceed registration")
+            sigma = credential.Issuer.issue(issuance_request.get("C"), username, attributes_list, server_sk)
+            return bytearray(jsonpickle.encode(sigma), 'utf-8')
+
+
+    def check_request_signature(self, server_pk, message, revealed_attributes, signature):
         """
 
         Args:
-            server_pk (byte[]): the server's public key (serialized)
+            server_pk (byte[]): the server's public key (serialized) and other parameters
             message (byte[]): The message to sign
             revealed_attributes (string): revealed attributes
             signature (bytes[]): user's autorization (serialized)
@@ -87,7 +122,7 @@ class Client:
         """Prepare a request to register a new account on the server.
 
         Args:
-            server_pk (byte[]): a server's public key (serialized)
+            server_pk (byte[]): a server's public key (serialized) and other parameters
             username (string): username
             attributes (string): user's attributes
 
@@ -100,29 +135,32 @@ class Client:
                 from prepare_registration to proceed_registration_response.
                 You need to design the state yourself.
         """
+
         server_pk = jsonpickle.decode(server_pk.decode("utf-8"))
 
         g = server_pk.get("g")
-        Yi = server_pk.get("pk")
-        G1= server_pk.get("G1")
+        Y = server_pk.get("pk")
+        G1 = server_pk.get("G1")
         t = petrelic.bn.Bn.from_num(rd.randint(1, G1.order()))
-        sk = petrelic.bn.Bn.from_num(rd.randint(1, G1.order()))
-        attributes_list = attributes.split(",")
+        attributes_list = attributes.split(',')
 
-
+        # secret key attribute of client
+        self.sk = petrelic.bn.Bn.from_num(rd.randint(1, G1.order()))
         
+        # create commit and zkp
         AnonCredential = credential.AnonCredential()
-        C, gamma, r = AnonCredential.create_issue_request([sk], G1, Yi, g, t, len(attributes_list))
+        C, gamma, r = AnonCredential.create_issue_request([self.sk], G1, Y, g, t, len(attributes_list))
         request = (jsonpickle.encode({"C": C, "gamma": gamma, "r": r})).encode('utf-8')
         private_state = {"C": C, "t": t}
         
+        print("Registration request created")
         return (request, private_state) 
 
     def proceed_registration_response(self, server_pk, server_response, private_state):
         """Process the response from the server.
 
         Args:
-            server_pk (byte[]): a server's public key (serialized)
+            server_pk (byte[]): a server's public key (serialized) and other parameters
             server_response (byte[]): the response from the server (serialized)
             private_state (private_state): state from the prepare_registration
             request corresponding to this response
@@ -139,7 +177,7 @@ class Client:
         """Signs the request with the clients credential.
 
         Arg:
-            server_pk (byte[]): a server's public key (serialized)
+            server_pk (byte[]): a server's public key (serialized) and other parameters
             credential (byte[]): client's credential (serialized)
             message (byte[]): message to sign
             revealed_info (string): attributes which need to be authorized
