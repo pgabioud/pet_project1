0b75064276b6cc6ca9188bd62a3dca19e9536430
Moodoo245 <justinas.sukaitis@epfl.ch>
2020-05-28 23:52:54 +0200

    DOOOONE

--- pet_project2/skeleton/part3/classifier/classifier.py
+++ pet_project2/skeleton/part3/classifier/classifier.py
@@ -6,12 +6,12 @@ from sklearn.model_selection import KFold
 
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
-
+import time
 
 def load_data():
 
     #import pickle
-    df = pd.read_pickle("data_wtime.pkl")
+    df = pd.read_pickle("data.pkl")
 
     #labels
     label = np.array(df['case'].values)
@@ -49,7 +49,7 @@ def load_data():
         length = lengths[i]
 
         #remove acks
-        t = length[abs(length) != 54]
+        t = length[abs(length) != 1]
         size_data.append(len(t))
         p = len(t)
         #incase filtering empties the row
@@ -83,12 +83,25 @@ def load_data():
     #to have (10000, 10)
     datum = datum.T
 
-    return datum, label
+    max_size = np.max(np.array(size_data))
+    data = np.zeros((len(df), max_size), np.float64)
+    cnt = 0
+    for i in df['lengths'].values:
+        i = i[abs(i) != 1]
+
+        data[cnt, :i.shape[0]] += i[:]
+
+        cnt += 1
+    times = np.expand_dims(np.array(times), 1)
+    data = np.concatenate((times, data), axis=1)
+    print(data.shape, max_size)
+    
+    return datum, data, label
 
 
 
 
-datum, label = load_data()
+datum, data, label = load_data()
 
 
 
@@ -96,17 +109,33 @@ datum, label = load_data()
 
 skf = KFold(n_splits=10)
 accuracy_model = []
-clf = RandomForestClassifier(
-    n_estimators=1000, max_features=7, max_depth = 20, n_jobs=-1, random_state=1)
 
 
+clf = RandomForestClassifier(n_estimators=200, max_features=0.7, max_depth = None, min_samples_leaf = 1, min_samples_split = 5, n_jobs=-1, random_state=1, bootstrap=False)
+i = 0
 for train_index, test_index in skf.split(datum, label):
-
     X_train, X_test = datum[train_index], datum[test_index]
     y_train, y_test = label[train_index], label[test_index]
 
-    print(X_train.shape)
+    '''
+    GRIDSEARCH
+    if i == 0:
+        for d in [10, 20, 30, 40, 50, None]:
+            for f in ['auto', 'sqrt', 0.3, 0.5, 0.6, 0.7, 0.8]:
+                for min_sleaf in [1, 2, 4, 5, 10]:
+                    for min_split in [2, 5, 10]:
+                        for boot in [True, False]:
+                            clf = RandomForestClassifier(n_estimators=200, max_features=f, max_depth = d, min_samples_leaf = min_sleaf, min_samples_split = min_split, n_jobs=-1, random_state=1)
+                            model = clf.fit(X_train, y_train)
+                            score = accuracy_score(y_test, model.predict(X_test), normalize=True)*100
 
+                            print(score, d, f, min_sleaf, min_split)
+
+                            i += 10
+    '''
+
+    print(X_train.shape)
+    t0 = time.time()
     model = clf.fit(X_train, y_train)
 
     #show feature importance for decisions
@@ -114,7 +143,27 @@ for train_index, test_index in skf.split(datum, label):
     print(importances)
 
     score = accuracy_score(y_test, model.predict(X_test), normalize=True)*100
-    print(score)
+    print(score, time.time()-t0)
+    accuracy_model.append(score)
+    print('------------------------------------------------------------------------')
+   
+print(accuracy_model)
+
+skf = KFold(n_splits=10)
+accuracy_model = []
+
+clf = RandomForestClassifier(n_estimators=500, max_features='sqrt', max_depth = 20, n_jobs=-1, random_state=1, bootstrap=False)
+for train_index, test_index in skf.split(data, label):
+    X_train, X_test = data[train_index], data[test_index]
+    y_train, y_test = label[train_index], label[test_index]
+    
+
+    print(X_train.shape)
+    t0 = time.time()
+    model = clf.fit(X_train, y_train)
+
+    score = accuracy_score(y_test, model.predict(X_test), normalize=True)*100
+    print(score, time.time()-t0)
     accuracy_model.append(score)
     print('------------------------------------------------------------------------')
    
